edd0a9ff555f126a1f99a3a0624f2b68b1e041cb
Junki Yoshida <j9141997@gmail.com>
2021-03-07 18:58:29 +0900

    feat: テンプレートリテラルでかく

--- components/popup.js
+++ components/popup.js
@@ -1,7 +1,8 @@
 class PopupElement extends HTMLElement {
   constructor() {
     super();
-    const template = document.querySelector('template#popup-tmpl').content.cloneNode(true);
+    const template = document.createElement('div');
+    template.innerHTML = this.template();
 
     const popup = template.querySelector('#popup');
     popup.addEventListener('click', function() {
@@ -18,6 +19,41 @@ class PopupElement extends HTMLElement {
     console.log(shadowRoot);
     shadowRoot.appendChild(template);
     shadowRoot.appendChild(button);
+  }
+
+  // テンプレートリテラルでマークアップ
+  template() {
+    return `
+      <style>
+        #popup {
+          display: none;
+          position: fixed;
+          background-color: rgba(0,0,0,0.3);
+          width: 100vw;
+          height: 100vh;
+          top: 0;
+          left: 0;
+        }
+        .dialog {
+          background-color: white;
+          width: 80vw;
+          margin: 100px auto 0 auto;
+          padding: 10px;
+          border-radius: 5px;
+        }
+        .title { font-weight: bold; }
+      </style>
+      <div id="popup">
+        <div class="dialog">
+          <div class="title">
+            <slot name="title"></slot>
+          </div>
+          <div class="content">
+            <slot name="content"></slot>
+          </div>
+        </div>
+      </div>  
+    `
   }
 }
 
